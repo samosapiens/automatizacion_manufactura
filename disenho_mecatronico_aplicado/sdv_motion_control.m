2028a989789705f23019ea2ccca4884715e59386
Santiago Álvarez Sepúlveda <42503265+samosapiens@users.noreply.github.com>
2018-12-16 10:41:08 -0500

    Add files via upload

--- disenho_mecatronico_aplicado/sdv_motion_control.m
+++ disenho_mecatronico_aplicado/sdv_motion_control.m
@@ -1,40 +1,41 @@
-% Iniciar Ejecución
+%% Diseño Axiomático Aplicado
+% Análisis ingenieril por la metodología de diseño axiomático para realiar
+% un modelo del robot SDV del laboratorio LabFabEx de la Universidad
+% Nacional de Colombia, sede Bogotá.
+
+% Inicio de Programa
 clear all
 close all
 clc
 
+%% Parámetros Mecánicos
+% Mecánicos
+n_ruedas = 4;           % Número de ruedas.
+r_rueda = 0.025;        % Radio de cada rueda [m]
+d_stpms = 0.2;          % Distancia entre STPMs [m]
+mod_engranajes = 0.5;   % Relación STPM
 
-%% Chasís
-n_ruedas = 4; % Número de ruedas.
-r_rueda = 0.025; % Radio de cada rueda [m]
-d_stpms = 0.2;  % Distancia entre STPMs [m]
+% Eléctricas
+v_max = 18;             % Voltaje máximo de alimentación [V]
 
+% Electromecánicos (Motores)
+J  = 0.01;              % Momento de inercia del eje del motor [kg*m^2]
+b  = 0.1;               % Fricción viscosa del motor [N*m*s]
+ke = 0.1;               % Constante de fuerza electromotriz [V/rad/sec]
+kt = 0.1;               % Constante de torque del motor [N*m/A]
+R  = 1.0;               % Resistencia eléctrica [Ohm]
+L  = 0.5;               % Inductancia eléctrica [H]
 
-%% Motores
-J  = 0.01;   % Momento de inercia del eje del motor [kg*m^2]
-b  = 0.1;    % Fricción viscosa del motor [N*m*s]
-ke = 0.01;   % Constante de fuerza electromotriz [V/rad/sec]
-kt = 0.01;   % Constante de torque del motor [N*m/A]
-R  = 1.0;    % Resistencia eléctrica [Ohm]
-L  = 0.5;    % Inductancia eléctrica [H]
+% Sistemas de Control
+ka = 5.0;              % Ganancia velocidad lineal
+kb = 5.0;              % Ganancia velocidad angular
+thresh_rho = 0.1;       % Cota máxima del error de posición [m]
+thresh_ang = 30*pi/180; % Cota máxima del error de orientación [rad]
 
-%% Sistema de Transmisión de Potencia Mecánica (STPM)
-mod_engranajes = 0.5;
+% Físicas
+W = 20.0;               % Peso del SDV [kg]
+Wi = W/n_ruedas;        % Peso soportado por cada rueda [kg]
 
-%% Referencias
-refI = 12;
-refD = 12;
-
-%% Voltaje
-v_max = 15;
-
-
-%% Conjunto Total
-W = 20.0; % Peso del SDV [kg]
-Wi = W/n_ruedas; % Peso soportado por cada rueda [kg]
-
-
-thresh_angle = 30*pi/180;
-thresh_rho = 0.1;
-ka = 10.0;
-kb = 10.0;
+%% Ejecutar el Modelo de Simulink
+open_system('SDV.slx');
+sim('SDV.slx');
